8aedc33d5a7046a54c80ca3a5dac55297eb7bb8c
admin <admin@707-12>
2026-03-25 11:11:44 +0700

    Add project files.

---
+++ Models/SchoolContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace SchoolAppCore.Models
+{
+	public class SchoolContext : DbContext
+	{
+		public SchoolContext(DbContextOptions<SchoolContext> options) :
+		base(options) {}
+		public DbSet<Department> Departments { get; set; }
+		public DbSet<Instructor> Instructors { get; set; }
+		public DbSet<Course> Courses { get; set; }
+		public DbSet<Student> Students { get; set; }
+		public DbSet<Enrollment> Enrollments { get; set; }
+	}
+
+	public class Department
+	{
+		public int DepartmentID { get; set; }
+		public string Name { get; set; }
+		public decimal Budget { get; set; }
+		public DateTime StartDate { get; set; }
+	}
+
+	public class Instructor
+	{
+		public int InstructorID { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public DateTime HireDate { get; set; }
+	}
+
+	public class Course
+	{
+		public int CourseID { get; set; }
+		public string Title { get; set; }
+		public int Credits { get; set; }
+		public int DepartmentID { get; set; }
+	}
+
+	public class Student
+	{
+		public int StudentID { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public DateTime EnrollmentDate { get; set; }
+	}
+
+	public class Enrollment
+	{
+		public int EnrollmentID { get; set; }
+		public int CourseID { get; set; }
+		public int StudentID { get; set; }
+		public decimal Grade { get; set; }
+	}
+}
---
+++ Pages/Enrollements/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SchoolAppCore.Models;
+
+namespace SchoolAppCore.Pages.Enrollements
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SchoolAppCore.Models.SchoolContext _context;
+
+        public IndexModel(SchoolAppCore.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Enrollment> Enrollment { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Enrollment = await _context.Enrollments.ToListAsync();
+        }
+    }
+}
